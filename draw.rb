36892db7e4dd29ddc9cd199b6af1ab4654e75756
James Brooking <jambroo@gmail.com>
2017-10-10 13:29:50 +0100

    fills!

--- draw.rb
+++ draw.rb
@@ -1,8 +1,37 @@
 $canvas = []
 
 def addX(x, y)
-  $canvas[y][x] = "X"
+  $canvas[y][x] = "#"
+  puts "STEP:"
   puts $canvas
+  puts ""
+
+  getNeighbours(x, y)
+end
+
+def isSet(x, y)
+  return $canvas[y][x] != " "
+end
+
+def setIfNotSet(x, y)
+  if !isSet(x, y)
+    addX(x, y)
+  end
+end
+
+def getNeighbours(x, y)
+  if x >= 1
+    setIfNotSet(x-1, y)
+  end
+  if x < $canvas[0].length
+    setIfNotSet(x+1, y)
+  end
+  if y >= 1
+    setIfNotSet(x, y-1)
+  end
+  if y < $canvas.length-1
+    setIfNotSet(x, y+1)
+  end
 end
 
 canvas = ""
@@ -16,6 +45,5 @@ f = File.open("canvas2", "r") do |f|
     end
     $canvas.push(canvas_raw[i][1..canvas_width-3])
   end
-
-  addX(2, 2)
+  getNeighbours(2, 2)
 end
