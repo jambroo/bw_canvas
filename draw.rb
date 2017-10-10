4fb9a1c47f6a32aebaa5d854199037222cb9b19b
James Brooking <jambroo@gmail.com>
2017-10-10 11:35:51 +0100

    use global var for canvas.

--- draw.rb
+++ draw.rb
@@ -1,15 +1,21 @@
+$canvas = []
+
+def addX(x, y)
+  $canvas[y][x] = "X"
+  puts $canvas
+end
+
 canvas = ""
 f = File.open("canvas2", "r") do |f|
   canvas_raw = f.readlines()
-  canvas = []
 
   canvas_width = canvas_raw[0].length
   for i in 1..canvas_raw.length
     if (canvas_raw[i+1] == nil || canvas_raw[i+1].empty?) && canvas_raw[i] == canvas_raw[0]
       break
     end
-    canvas.push(canvas_raw[i][1..canvas_width-3])
+    $canvas.push(canvas_raw[i][1..canvas_width-3])
   end
 
-  puts canvas
+  addX(2, 2)
 end
